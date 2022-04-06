9612bf3c71a1d079a4765ebb59288a4af97c354f
student <student@PC-630.urel.feec.vutbr.cz>
2022-04-06 10:42:28 +0200

    .

--- labs/05-ffs/flip_flops/flip_flops.srcs/sources_1/new/t_ff_rst.vhd
+++ labs/05-ffs/flip_flops/flip_flops.srcs/sources_1/new/t_ff_rst.vhd
@@ -2,9 +2,9 @@
 -- Company: 
 -- Engineer: 
 -- 
--- Create Date: 03/16/2022 09:45:41 AM
+-- Create Date: 03/10/2022 01:13:19 PM
 -- Design Name: 
--- Module Name: t_ff_rst - Behavioral
+-- Module Name: d_ff_rst - Behavioral
 -- Project Name: 
 -- Target Devices: 
 -- Tool Versions: 
@@ -32,12 +32,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity t_ff_rst is
---  Port ( );
+    Port ( clk  : in STD_LOGIC;
+           rst  : in STD_LOGIC;
+           t    : in STD_LOGIC;
+           q    : out STD_LOGIC;
+           q_bar : out STD_LOGIC);
 end t_ff_rst;
 
 architecture Behavioral of t_ff_rst is
-
+signal s_q : std_logic;
 begin
-
-
-end Behavioral;
+    --------------------------------------------------------
+    -- p_d_ff_rst:
+    -- D type flip-flop with a high-active sync reset,
+    -- rising-edge clk.
+    -- q(n+1) = d
+    --------------------------------------------------------
+    p_t_ff_rst : process(clk)
+    begin
+        if rising_edge(clk) then  -- Synchronous process
+            if(rst = '1') then
+                s_q <= '0';
+                q     <= '0';
+                q_bar <= not '1';
+            else
+                s_q     <= (not (t) and s_q) or ( t and not (s_q));
+                q       <= s_q;
+                q_bar   <= not s_q;
+             end if;
+        end if;
+    end process p_t_ff_rst;
+end architecture Behavioral;
